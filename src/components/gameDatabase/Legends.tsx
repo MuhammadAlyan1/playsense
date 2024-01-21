03edc3caaeae8f65688cca54781554f607d0644a
Muhammad Alyan <alyan0332@gmail.com>
2024-01-22 01:07:22 +0500

    add game database frontend

---
+++ src/components/gameDatabase/Legends.tsx
@@ -0,0 +1,176 @@
+import AshIcon from '../../assets/icons/characters/ash.svg?react';
+import BangaloreIcon from '../../assets/icons/characters/bangalore.svg?react';
+import BloodhoundIcon from '../../assets/icons/characters/bloodhound.svg?react';
+import BallisticIcon from '../../assets/icons/characters/ballistic.svg?react';
+import CausticIcon from '../../assets/icons/characters/caustic.svg?react';
+import CryptoIcon from '../../assets/icons/characters/crypto.svg?react';
+import CatalystIcon from '../../assets/icons/characters/catalyst.svg?react';
+import ConduitIcon from '../../assets/icons/characters/conduit.svg?react';
+import FuseIcon from '../../assets/icons/characters/fuse.svg?react';
+import GibraltarIcon from '../../assets/icons/characters/gibraltar.svg?react';
+import HorizonIcon from '../../assets/icons/characters/horizon.svg?react';
+import LifelineIcon from '../../assets/icons/characters/lifeline.svg?react';
+import LobaIcon from '../../assets/icons/characters/loba.svg?react';
+import MirageIcon from '../../assets/icons/characters/mirage.svg?react';
+import MadMaggieIcon from '../../assets/icons/characters/mad-maggie.svg?react';
+import NewcastleIcon from '../../assets/icons/characters/newcastle.svg?react';
+import OctaneIcon from '../../assets/icons/characters/octane.svg?react';
+import PathfinderIcon from '../../assets/icons/characters/pathfinder.svg?react';
+import RampartIcon from '../../assets/icons/characters/rampart.svg?react';
+import RevenantIcon from '../../assets/icons/characters/revenant.svg?react';
+import WattsonIcon from '../../assets/icons/characters/wattson.svg?react';
+import WraithIcon from '../../assets/icons/characters/wraith.svg?react';
+import ValkyrieIcon from '../../assets/icons/characters/valkyrie.svg?react';
+import VantageIcon from '../../assets/icons/characters/vantage.svg?react';
+import SeerIcon from '../../assets/icons/characters/seer.svg?react';
+
+const Legends = () => {
+  return (
+    <div className="legends">
+      <h2 className="legends__sub-heading">Legend</h2>
+      <div className="legends__legends-container">
+        {legends.map((legend) => {
+          return (
+            <div
+              title={legend.name}
+              className={`icon-button`}
+              key={legend.name}
+            >
+              <legend.icon className="icon" />
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Legends;
+
+const legends = [
+  {
+    id: '1',
+    name: 'Ash',
+    icon: AshIcon
+  },
+  {
+    id: '2',
+    name: 'Bangalore',
+    icon: BangaloreIcon
+  },
+  {
+    id: '3',
+    name: 'Ballistic',
+    icon: BallisticIcon
+  },
+  {
+    id: '4',
+    name: 'Bloodhound',
+    icon: BloodhoundIcon
+  },
+  {
+    id: '5',
+    name: 'Caustic',
+    icon: CausticIcon
+  },
+  {
+    id: '6',
+    name: 'Crypto',
+    icon: CryptoIcon
+  },
+  {
+    id: '7',
+    name: 'Catalyst',
+    icon: CatalystIcon
+  },
+  {
+    id: '8',
+    name: 'Conduit',
+    icon: ConduitIcon
+  },
+  {
+    id: '9',
+    name: 'Fuse',
+    icon: FuseIcon
+  },
+  {
+    id: '10',
+    name: 'Gibraltar',
+    icon: GibraltarIcon
+  },
+  {
+    id: '11',
+    name: 'Horizon',
+    icon: HorizonIcon
+  },
+  {
+    id: '12',
+    name: 'Lifeline',
+    icon: LifelineIcon
+  },
+  {
+    id: '13',
+    name: 'Loba',
+    icon: LobaIcon
+  },
+  {
+    id: '14',
+    name: 'Mad Maggie',
+    icon: MadMaggieIcon
+  },
+  {
+    id: '15',
+    name: 'Mirage',
+    icon: MirageIcon
+  },
+  {
+    id: '16',
+    name: 'Newcastle',
+    icon: NewcastleIcon
+  },
+  {
+    id: '17',
+    name: 'Octane',
+    icon: OctaneIcon
+  },
+  {
+    id: '18',
+    name: 'Pathfinder',
+    icon: PathfinderIcon
+  },
+  {
+    id: '19',
+    name: 'Rampart',
+    icon: RampartIcon
+  },
+  {
+    id: '20',
+    name: 'Revenant',
+    icon: RevenantIcon
+  },
+  {
+    id: '21',
+    name: 'Seer',
+    icon: SeerIcon
+  },
+  {
+    id: '22',
+    name: 'Valkyrie',
+    icon: ValkyrieIcon
+  },
+  {
+    id: '23',
+    name: 'Vantage',
+    icon: VantageIcon
+  },
+  {
+    id: '24',
+    name: 'Wattson',
+    icon: WattsonIcon
+  },
+  {
+    id: '25',
+    name: 'Wraith',
+    icon: WraithIcon
+  }
+];
--- src/components/gameDatabase/index.tsx
+++ src/components/gameDatabase/index.tsx
@@ -1,7 +1,12 @@
+import Legends from './Legends';
+import Weapons from './Weapons';
+
 const GameDatabase = () => {
   return (
     <div className="game-database">
       <h1 className="game-database__heading">Game Database</h1>
+      <Legends />
+      <Weapons />
     </div>
   );
 };
