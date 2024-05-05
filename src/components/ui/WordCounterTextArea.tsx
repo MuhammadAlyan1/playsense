44ad031441e9358165d86b6dbd5fd194a308b602
Muhammad Alyan <alyan0332@gmail.com>
2024-05-05 23:52:24 +0500

    Add textarea with word counter component

---
+++ src/components/ui/WordCounterTextArea.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+type WordCounterTextAreaType = {
+  state: string;
+  setState: (value: string) => void;
+  MAX_WORD_COUNT?: number;
+  placeholder?: string;
+  rows?: number;
+};
+
+const WordCounterTextArea: React.FC<WordCounterTextAreaType> = ({
+  state,
+  setState,
+  MAX_WORD_COUNT = 500,
+  placeholder = '',
+  rows = 7
+}) => {
+  return (
+    <div className="word-counter-textarea">
+      <textarea
+        required
+        className="word-counter-textarea__textarea"
+        rows={rows}
+        value={state}
+        onChange={(e) => setState(e.target.value)}
+        maxLength={MAX_WORD_COUNT}
+        placeholder={
+          placeholder ||
+          'Craft your gaming tales, tips, and thoughts here. Let the community in on your gaming journey.'
+        }
+      />
+      <p className="word-counter-textarea__word-counter">{`${state.length}/${MAX_WORD_COUNT}`}</p>
+    </div>
+  );
+};
+
+export default WordCounterTextArea;
