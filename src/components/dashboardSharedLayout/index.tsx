3a585a6bcb7013951e7a5e99ad12219f80573b7f
Muhammad Alyan <alyan0332@gmail.com>
2024-07-13 21:48:49 +0500

    Fetch credentials on dashboard

--- src/components/dashboardSharedLayout/index.tsx
+++ src/components/dashboardSharedLayout/index.tsx
@@ -1,25 +1,38 @@
-// import React, { useEffect, useContext } from 'react';
-import { Outlet } from 'react-router-dom';
-// import { useLocation, useNavigate } from 'react-router-dom';
+import { Outlet, useNavigate } from 'react-router-dom';
 import DashboardNavigation from '../dashboardNavigation';
 import Navbar from '../navbar';
+import useAuth from '../../hooks/useAuth';
+import { useEffect } from 'react';
+import axios from '../../api/axios';
 
 const DashboardSharedLayout = () => {
-  // const {
-  //   state: { user }
-  // } = useContext(AppContext);
-  // const location = useLocation();
-  // const navigate = useNavigate();
-  // const { userID } = user;
+  const navigate = useNavigate();
+  const auth = useAuth();
+  useEffect(() => {
+    const setAuth = auth?.setAuth && auth?.setAuth;
+    const checkAuth = async () => {
+      try {
+        const response = await axios.get(
+          `${import.meta.env.VITE_BASE_URL}/jwt/check-auth`,
+          {
+            withCredentials: true
+          }
+        );
 
-  // useEffect(() => {
-  //   if (location.pathname === '/signin') return;
-  //   if (location.pathname === '/signup') return;
+        if (response?.data?.success === true) {
+          console.log('Succesfully signed in');
+          response.data.data && setAuth && setAuth(response.data.data);
+        } else if (response?.data?.success === false) {
+          console.log(response.data.message);
+        }
+      } catch (error) {
+        console.log(error);
+        navigate('/signin');
+      }
+    };
 
-  //   if (!userID) {
-  //     navigate('/signin');
-  //   }
-  // }, [location]);
+    checkAuth();
+  }, []);
 
   return (
     <div className="dashboard-shared-layout">
