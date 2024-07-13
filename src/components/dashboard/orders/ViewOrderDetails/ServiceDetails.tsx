ae61925fd501ee9481efcdc5576881fac33eb3f6
Muhammad Alyan <alyan0332@gmail.com>
2024-07-13 21:49:15 +0500

    Create service details component

---
+++ src/components/dashboard/orders/ViewOrderDetails/ServiceDetails.tsx
@@ -0,0 +1,50 @@
+import moment from 'moment';
+import { OrderType } from '../../../../types/OrderType';
+import { FaStar } from 'react-icons/fa';
+
+type ServiceDetailsType = {
+  order: OrderType | undefined;
+};
+
+const ServiceDetails: React.FC<ServiceDetailsType> = ({ order }) => {
+  if (!order) return;
+
+  return (
+    <>
+      <p className="view-order__label">Seller</p>
+      <p
+        className={`view-order__value view-order__badge view-order__badge--blue`}
+      >
+        {order?.sellerId?.username}
+      </p>
+      <p className="view-order__label">Title</p>
+      <p className="view-order__value view-order__badge view-order__badge--info">
+        {order?.serviceId?.title}
+      </p>
+      <p className="view-order__label">Price</p>
+      <p className="view-order__value view-order__badge view-order__badge--green">
+        ${order?.serviceId?.price}
+      </p>
+
+      <p className="view-order__label">Rating</p>
+      <p
+        className={`view-order__value view-order__badge view-order__badge--rating view-order__rating `}
+      >
+        {order.serviceId?.rating || 0}{' '}
+        <FaStar className="view-order__rating-icon" />
+      </p>
+      <p className="view-order__label">Total Sales</p>
+      <p
+        className={`view-order__value view-order__badge view-order__badge--blue`}
+      >
+        {order.serviceId.totalSales || 0}
+      </p>
+      <p className="view-order__label">Created At</p>
+      <p className="view-order__value view-order__badge view-order__badge--blue">
+        {moment(order?.serviceId?.createdAt).format('dddd, MMMM Do YYYY')}
+      </p>
+    </>
+  );
+};
+
+export default ServiceDetails;
