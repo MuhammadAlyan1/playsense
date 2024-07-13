c9fde674f0e4a3eb648d212282b748396f00b877
Muhammad Alyan <alyan0332@gmail.com>
2024-07-13 22:27:51 +0500

    add disabled prop in wordCounterTextArea

--- src/components/ui/WordCounterTextArea.tsx
+++ src/components/ui/WordCounterTextArea.tsx
@@ -6,6 +6,7 @@ type WordCounterTextAreaType = {
   MAX_WORD_COUNT?: number;
   placeholder?: string;
   rows?: number;
+  isDisabled?: boolean;
 };
 
 const WordCounterTextArea: React.FC<WordCounterTextAreaType> = ({
@@ -13,7 +14,8 @@ const WordCounterTextArea: React.FC<WordCounterTextAreaType> = ({
   setState,
   MAX_WORD_COUNT = 500,
   placeholder = '',
-  rows = 7
+  rows = 7,
+  isDisabled = false
 }) => {
   return (
     <div className="word-counter-textarea">
@@ -28,6 +30,7 @@ const WordCounterTextArea: React.FC<WordCounterTextAreaType> = ({
           placeholder ||
           'Craft your gaming tales, tips, and thoughts here. Let the community in on your gaming journey.'
         }
+        disabled={isDisabled}
       />
       <p className="word-counter-textarea__word-counter">{`${state.length}/${MAX_WORD_COUNT}`}</p>
     </div>
