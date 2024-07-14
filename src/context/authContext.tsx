7e9bbe8b682714cab89277bab9090e1bef1ae553
Muhammad Alyan <alyan0332@gmail.com>
2024-07-14 22:59:56 +0500

    Add signout functionality

--- src/context/authContext.tsx
+++ src/context/authContext.tsx
@@ -10,6 +10,7 @@ import { ProfileType } from '../types/ProfileType';
 type AuthContextType = {
   auth: ProfileType;
   setAuth: Dispatch<SetStateAction<ProfileType>>;
+  signOut: () => void;
 };
 
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
@@ -42,11 +43,41 @@ export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
     mousepad: '',
     createdAt: '',
     updatedAt: '',
-    __v: 0
+    __v: 0,
+    friends: []
   });
 
+  const signOut = () => {
+    setAuth({
+      _id: '',
+      userId: '',
+      username: '',
+      roles: ['user'],
+      platform: 'pc',
+      bio: '',
+      profilePicture:
+        'https://res.cloudinary.com/dcc8txmdw/image/upload/v1705819981/playsense-profile/vgiadrbjgmgd6qhdwoy2.jpg',
+      banner:
+        'https://res.cloudinary.com/dcc8txmdw/image/upload/v1705819985/playsense-profile/bozzoqaani8tgamqdqfg.jpg',
+      country: 'Pakistan',
+      twitchUrl: 'https://www.twitch.tv/',
+      youtubeUrl: 'https://www.youtube.com/',
+      twitterUrl: 'https://twitter.com/',
+      discordUsername: 'PlaySense',
+      monitor: '',
+      keyboard: '',
+      headphones: '',
+      mouse: '',
+      mousepad: '',
+      createdAt: '',
+      updatedAt: '',
+      __v: 0,
+      friends: []
+    });
+  };
+
   return (
-    <AuthContext.Provider value={{ auth, setAuth }}>
+    <AuthContext.Provider value={{ auth, setAuth, signOut }}>
       {children}
     </AuthContext.Provider>
   );
