1ef847910392bfb4bfd890eaef1f180b9ba53315
Muhammad Alyan <alyan0332@gmail.com>
2024-07-24 01:04:23 +0500

    Add withCredentials true

--- src/socket.ts
+++ src/socket.ts
@@ -1,5 +1,6 @@
 import { io } from 'socket.io-client';
 export const socket = io(import.meta.env.VITE_BACKEND_URL, {
   autoConnect: false,
-  transports: ['websocket', 'polling']
+  transports: ['websocket', 'polling'],
+  withCredentials: true
 });
