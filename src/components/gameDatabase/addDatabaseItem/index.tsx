731620752beab3c759dd1ed3bbca906f0a04ba5f
Muhammad Alyan <alyan0332@gmail.com>
2024-08-07 23:27:28 +0500

    Create component to add items to game database

---
+++ src/components/gameDatabase/addDatabaseItem/index.tsx
@@ -0,0 +1,51 @@
+import { useState } from 'react';
+import Modal from '../../ui/Modal';
+import Select from '../../ui/Select';
+import GameSvg from '../../../assets/icons/game.svg?react';
+import TypeSvg from '../../../assets/icons/tag.svg?react';
+import AddWeapon from './addWeapon';
+import AddLegend from './AddLegend';
+
+type AddDatabaseItem = {
+  isModalOpen: boolean;
+  setIsModalOpen: (value: boolean) => void;
+};
+
+const AddDatabaseItem: React.FC<AddDatabaseItem> = ({
+  isModalOpen,
+  setIsModalOpen
+}) => {
+  const [game, setGame] = useState('apex legends');
+  const [type, setType] = useState('weapon');
+
+  return (
+    <Modal isOpen={isModalOpen} onClose={setIsModalOpen} title={''}>
+      <div className="add-database-item add-database-item">
+        <h1 className="add-database-item__heading">
+          Contribute to Game Database
+        </h1>
+        <p className="add-database-item__label">Game</p>
+        <Select
+          state={game}
+          setState={setGame}
+          selectOptions={['Apex Legends', 'Warzone', 'CSGO']}
+          Icon={GameSvg}
+        />
+        <p className="add-database-item__label">Type</p>
+        <Select
+          state={type}
+          setState={setType}
+          selectOptions={['weapon', 'legend']}
+          Icon={TypeSvg}
+        />
+        {type === 'weapon' ? (
+          <AddWeapon game={game} setIsModalOpen={setIsModalOpen} />
+        ) : (
+          <AddLegend game={game} setIsModalOpen={setIsModalOpen} />
+        )}
+      </div>
+    </Modal>
+  );
+};
+
+export default AddDatabaseItem;
