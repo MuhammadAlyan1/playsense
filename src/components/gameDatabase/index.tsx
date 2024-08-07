09cac3b76e17d8a08b813a2784657a73698a5a6a
Muhammad Alyan <alyan0332@gmail.com>
2024-08-07 23:27:45 +0500

    Add modal to add items to game database

--- src/components/gameDatabase/index.tsx
+++ src/components/gameDatabase/index.tsx
@@ -1,12 +1,29 @@
+import { useState } from 'react';
 import Legends from './Legends';
 import Weapons from './Weapons';
+import AddDatabaseItem from './addDatabaseItem';
 
 const GameDatabase = () => {
+  const [isAddItemModalOpen, setIsAddItemModalOpen] = useState(false);
   return (
     <div className="game-database">
-      <h1 className="game-database__heading">Game Database</h1>
+      <div className="game-database__header">
+        <h1 className="game-database__heading">Game Database</h1>
+        <button
+          className="game-database__button"
+          onClick={() => {
+            setIsAddItemModalOpen((prev) => !prev);
+          }}
+        >
+          Contribute
+        </button>
+      </div>
       <Legends />
       <Weapons />
+      <AddDatabaseItem
+        isModalOpen={isAddItemModalOpen}
+        setIsModalOpen={setIsAddItemModalOpen}
+      />
     </div>
   );
 };
